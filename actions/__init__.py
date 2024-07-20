45ab351d7d69f9983ea1ec02d5bbcf0c0a176f78
Sahil Barbade <sahilbarbade.ee101@gmail.com>
2024-07-20 23:26:52 +0530

    update actions.py with RAG using Gemini

---
+++ actions/__init__.py
@@ -0,0 +1,2 @@
+# In your __init__.py, make sure to import the ActionQueryRAG class
+from .actions import ActionQueryRAG
--- actions/actions.py
+++ actions/actions.py
@@ -3,25 +3,71 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+# actions/actions.py
 
+api_key = "AIzaSyBkxwWO74DmFDgvXwAGPUvwymTRhoYWrtM"
 
-# This is a simple example for a custom action which utters "Hello World!"
+import os
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_chroma import Chroma
+import google.generativeai as genai
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import chromadb
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+# Set Hugging Face token
+os.environ["API_KEY"] = api_key
+genai.configure(api_key=os.environ["API_KEY"])
+
+class ActionQueryRAG(Action):
+
+    def name(self) -> str:
+        return "action_query_rag"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict) -> list:
+        
+        # Load the document and setup the environment (you might want to optimize this)
+        book_path = "/Users/sahilbarbade/RAG/document/RASA/books/ugrulebook.pdf"
+        loader = PyPDFLoader(book_path)
+        document = loader.load()
+
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 100,
+            add_start_index = True
+        )
+
+        chunks = splitter.split_documents(document)
+
+        embedding_function = HuggingFaceEmbeddings()
+        client = chromadb.EphemeralClient()
+
+        db = Chroma.from_documents(
+            chunks, embedding_function, client=client, collection_name="iitb_collection"
+        )
+
+        # Get the user query
+        query = tracker.latest_message.get('text')
+
+        docs = db.similarity_search(query, k = 2)
+        message = "\n\n -- \n\n".join([l.page_content for l in docs])
+
+        PROMPT_TEMPLATE = """Answer the questions based on following context: \n\n {context} \n --- \n\n Answer the questions based on above context \n\n {query} """
+        prompts = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        prompt = prompts.format(context = message, query = query)
+
+        # Initialize google gemini flash model for question-answering pipeline
+        model = genai.GenerativeModel('gemini-1.5-flash')
+        response = model.generate_content(prompt)
+
+        # Send the response back to the user
+        dispatcher.utter_message(text=response.text)
+
+        return []
+
